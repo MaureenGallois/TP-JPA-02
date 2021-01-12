352beb80a823f5f149a312b30f27efdec1130446
Maureen GALLOIS <m.gallois@unyc.io>
2021-01-12 11:26:04 +0100

    modification de livre

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,12 +9,21 @@ public class App {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu-test-jpa");
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
+        //ajout d'un livre
         Livre newLivre = new Livre();
         newLivre.setTitre("Ratatouille");
         newLivre.setAuteur("Auguste Gusteau");
         em.persist(newLivre);
-        Livre livre = em.find(Livre.class, 6);
-        System.out.println(livre);
+
+        //modification d'un livre
+        Livre livre = em.find(Livre.class, 5);
+        if(livre != null){
+            livre.setTitre("Du plaisir dans la cuisine");
+        }
+
+        //recherche d'un livre par son id
+        Livre findLivre = em.find(Livre.class, 5);
+        System.out.println(findLivre);
         em.getTransaction().commit();
         em.close();
         emf.close();
