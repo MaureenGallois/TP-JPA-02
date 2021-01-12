43595c64c3426c9c0495980ca8c27b9f97b4288d
Maureen GALLOIS <m.gallois@unyc.io>
2021-01-12 11:45:17 +0100

    requête jpql extraction en fonction du titre

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,7 @@ import domain.Livre;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 public class App {
     public static void main(String[] args) {
@@ -26,6 +27,11 @@ public class App {
         if(deleteLivre != null){
             em.remove(deleteLivre);
         }
+
+        //Faire une requête JPQL pour extraire de la base un livre en fonction de son titre
+        TypedQuery<Livre> selectQuery = em.createQuery("select l from Livre l where l.titre ='Vingt mille lieues sous les mers'", Livre.class);
+        Livre selectLivre = selectQuery.getResultList().get(0);
+        System.out.println(selectLivre);
 
         //recherche d'un livre par son id
         Livre findLivre = em.find(Livre.class, 5);
